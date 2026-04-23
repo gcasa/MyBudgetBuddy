23f4275e7d222b9223492f5616fe50d29103706e
Gregory Casamento <greg.casamento@gmail.com>
2026-04-23 11:55:44 -0400

    Add DatabaseException code.

---
+++ src/main/java/com/mybudgetbuddy/infrastructure/database/DatabaseException.java
@@ -0,0 +1,15 @@
+package com.mybudgetbuddy.infrastructure.database;
+
+/**
+ * Custom exception for database operations
+ */
+public class DatabaseException extends RuntimeException {
+    
+    public DatabaseException(String message) {
+        super(message);
+    }
+    
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
